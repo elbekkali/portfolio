dc838fc086f8b082d1acd3576d337d8b44c5a692
elbekkali <elbekkali23mohamed@gmail.com>
2026-05-05 00:46:49 +0200

    correction bug environements

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "TA_CLE_API",
-    authDomain: "...",
-    projectId: "...",
-    storageBucket: "...",
-    messagingSenderId: "...",
-    appId: "..."
+    apiKey: "AIzaSyAMIlm3W8Q5fOaQo9491LH5Phb6CKzHZTk",
+    authDomain: "portfolio-2308.firebaseapp.com",
+    projectId: "portfolio-2308",
+    storageBucket: "portfolio-2308.firebasestorage.app",
+    messagingSenderId: "631154532000",
+    appId: "1:631154532000:web:3bca32516fdff9ece433c5"
   }
 };
